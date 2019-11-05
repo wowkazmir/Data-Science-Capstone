47dd6a7b73e721e31f136639bcacc992272cfc6d
Amyas K. Walji <43777661+wowkazmir@users.noreply.github.com>
2019-11-05 21:22:25 +0100

    Delete global.R

--- Docker_ShinyApp/app/global.R
+++
@@ -1,6 +0,0 @@
-library(shiny)
-
-uniGrams <- readRDS("uniGrams_probk.rds")
-biGrams <- readRDS("biGrams_probk.rds")
-triGrams <- readRDS("triGrams_probk.rds")
-quadGrams <- readRDS("quadGrams_probk.rds")
